843d476893b5e0da61705e679a5db2bbc571af15
Kirbyy <81638158+Kirbyy1@users.noreply.github.com>
2023-09-06 00:12:22 +0300

    Create exceptions.py

---
+++ RiotAuth/exceptions.py
@@ -0,0 +1,14 @@
+class UnableToRetrieveCaptchaToken(Exception):
+    pass
+
+
+class UnableToRetrieveLoginToken(Exception):
+    pass
+
+
+class UnableToRetrieveLoginCookies(Exception):
+    pass
+
+
+class UnableToSolveCaptcha(Exception):
+    pass
